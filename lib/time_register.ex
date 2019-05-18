0d31b7d2c3d9e1a4d46a99fdfa0256ffe8c032ff
Ignacio Aliende García <ialiendeg@gmail.com>
2019-05-18 09:30:37 +0100

    initial project structure

---
+++ lib/time_register.ex
@@ -0,0 +1,18 @@
+defmodule TimeRegister do
+  @moduledoc """
+  Documentation for TimeRegister.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TimeRegister.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/time_register_test.exs
@@ -0,0 +1,8 @@
+defmodule TimeRegisterTest do
+  use ExUnit.Case
+  doctest TimeRegister
+
+  test "greets the world" do
+    assert TimeRegister.hello() == :world
+  end
+end
